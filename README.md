7da24aba7b60da9d2b6de97db5bbf76843d034ae
Rafi <rafimaster9@gmail.com>
2022-07-06 16:18:37 +0700

    final

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 # vulnerable active directory lab
-===========================
 
-    UNDER CONSTRUCTION !!!
+Hi, kali ini saya akan memberikan lab untuk belajar tentang active directory.
 
-===========================
+`dificulity : easy `
+
+## cara setup
+
+1. [Download]() repo ini
+2. Pastikan anda memiliki virtualbox
+3. Install image windows server tersebut
+4. Login dengan password `P@ssw0rd`
+5. Masukan angka 8 lalu setting network
+    ![](https://xpertstec.com/wp-content/uploads/2022/03/Welcome-to-window-core-server.png?ezimgfmt=ng:webp/ngcb3)
+6. Ketik 1 untuk memilih 
+    ![](https://xpertstec.com/wp-content/uploads/2022/03/Available-adapter-core-server-1.png?ezimgfmt=ng:webp/ngcb3)
+7. Ketik 1 lalu, Ganti ip menggunakan ip static ***sesuaikan dengan network anda***
+    ![](https://xpertstec.com/wp-content/uploads/2022/03/Network-adapter-settings-core-server.png?ezimgfmt=ng:webp/ngcb3)
+8. Setelah itu, ketik 2 dan masukan ip anda sebagai dns
+   ![](https://xpertstec.com/wp-content/uploads/2022/03/DNS-settings-core-server.png?ezimgfmt=ng:webp/ngcb3)
+9.  Masuk ke menu utama dan ketik 15 untuk masuk ke powershell
+10. Masukan perintah ini
+```
+C:\Windows\Task\setup.ps1
+```
+## kerentanan
+
+- celah ada pada password yang lemah 
+
+## writeup & reference
+
+- [Medium]() belom buat :D
+- [John Hammond youtube](https://www.youtube.com/watch?v=WPnFnPkOWIg&t=10s)
+------------------
+
+*note : mungkin lab ini tidak sama seperti di real life karena lab ini untuk pemula dengan tingkat kesulitan rendah*
+
+### **Selamat belajar!**
---
+++ setup.ps1
@@ -0,0 +1,100 @@
+# run this script to initial setup
+function ShowBanner {
+    $banner  = @()
+    $banner+= $Global:Spacing + ''                                               
+    $banner+= $Global:Spacing + '======================================'
+    $banner+= $Global:Spacing + '     __                            '
+    $banner+= $Global:Spacing + '    / /____ ________ ____ ____ __ '
+    $banner+= $Global:Spacing + '   /  _/ // / __/ // / // / // / '
+    $banner+= $Global:Spacing + '  /_/\_\\_, /_/  \_,_/\_,_/\_,_/  '
+    $banner+= $Global:Spacing + '       /___/                     '
+    $banner+= $Global:Spacing + ''
+    $banner+= $Global:Spacing + ''
+    $banner+= $Global:Spacing + 'vulnerable active directory by kyruuu'
+    $banner+= $Global:Spacing + '======================================'
+    $banner | foreach-object {
+        Write-Host $_ -ForegroundColor @('Cyan')
+    }                             
+}
+
+$groups = [System.Collections.ArrayList](Get-Content "groups.txt")
+$firstnames = [System.Collections.ArrayList](Get-Content "firstname.txt")
+$lastnames = [System.Collections.ArrayList](Get-Content "lastname.txt")
+$passwords = [System.Collections.ArrayList](Get-Content "password.txt")
+
+
+$group = @()
+$user = @()
+
+
+$max_groups = 7
+for ($i = 0; $i -lt $max_groups; $i++){
+    $g = (Get-Random -InputObject $groups)
+    $group += @{"name" = "$g"}
+    $groups.Remove($g)
+}
+
+$max_users = 64
+for ($i = 0; $i -lt $max_users; $i++){
+    $f = (Get-Random -InputObject $firstnames)
+    $l = (Get-Random -InputObject $lastnames)
+    $p = (Get-Random -InputObject $passwords)
+    $userd = @{
+        "name" = "$f $l"
+        "password" = "$p"
+        "groups" = @((Get-Random -InputObject $group).name)
+    }
+    $user += $userd
+    $passwords.Remove($p)
+    $firstnames.Remove($f)
+    $lastnames.Remove($l)
+}
+
+function CreateADgroup(){
+    param( [Parameter(Mandatory=$true)] $groupObject )
+    
+    $name = $groupObject.name
+
+    New-ADGroup -Name $name -GroupScope Global
+}
+
+function CreateADUser() {
+    param( [Parameter(Mandatory=$true)] $userObject )    
+    
+
+    $name = $userObject.name;
+    $firstname  = $userObject.name.split(' ')[0]
+    $lastname = $userObject.name.split(' ')[1]
+    $username = "{0}{1}" -f ($firstname[0] , $lastname);
+    $SamAccountName = $username.tolower();
+    $principalname = $username.tolower();
+    $password = $userObject.password;
+
+    New-ADUser -Name $name -GivenName $firstname -Surname $lastname -SamAccountName $SamAccountName -UserPrincipalName $principalname@$Global:Domain -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -PassThru | Enable-ADAccount
+
+    foreach ($group in $userObject.groups) {
+        try {
+            Get-ADGroup -Identity $group
+            Add-ADGroupMember -Identity $group -Members $SamAccountName
+        }
+        catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
+            Write-Warning "Failed to add $SamAccountName to $group cz the group doesn't exist"
+        }
+    }
+}
+
+$Global:Domain = "kyruuu.com"
+
+ShowBanner
+
+echo "creating all random groups and users ..."
+
+foreach( $i in $group){
+    CreateADgroup $i 
+}
+
+foreach( $i in  $user){
+    CreateADUser $i
+}
+
+echo "done, you're ready to go!"
